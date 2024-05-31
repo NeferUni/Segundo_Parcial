dd2f4b805cd88822c3de010d23a24c568dfa1c39
Nefer Sneyder <nefersneyderrp@ufps.edu.co>
2024-05-31 12:09:40 -0500

    pequeño cambio

--- src/main/java/co/edu/ufps/entity/Favorito.java
+++ src/main/java/co/edu/ufps/entity/Favorito.java
@@ -8,10 +8,12 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "favorito")
 public class Favorito {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
